89eb7bd8489faf11f8ded7d0fc140a14c6979ce1
Toaster <SanderVermeulen1.SV@gmail.com>
2020-09-27 20:44:48 +0200

    27/09/2020

--- GUI.py
+++ GUI.py
@@ -1,31 +1,32 @@
 import tkinter as tk
 from tkinter import filedialog, Text
 from yaml import load, FullLoader
+
+from Components import Buttons
 import sys
 
-# region Config
 window_title = "Stock Market AI"
-window_width = 800
-window_height = 500
-background_color = "#091726"
-navbar_color = "#0e0926";
+window_width = 1280
+window_height = 720
+background_color = "#356e78"
+navbar_color = "#063842"
 
-with open("config.yaml") as config:
-    data = load(config, Loader=FullLoader)
-
-    if "window_title" in data:
-        window_title = data["window_title"]
-
-    if "start_window_height" in data:
-        window_height = data["start_window_height"]
-    if "start_window_width" in data:
-        window_width = data["start_window_width"]
-
-    if "background_color" in data:
-        background_color = data["background_color"]
-    if "navbar_color" in data:
-        navbar_color = data["navbar_color"]
-# endregion
+#
+# with open("config.yaml") as config:
+#     data = load(config, Loader=FullLoader)
+#
+#     if "window_title" in data:
+#         window_title = data["window_title"]
+#
+#     if "start_window_height" in data:
+#         window_height = data["start_window_height"]
+#     if "start_window_width" in data:
+#         window_width = data["start_window_width"]
+#
+#     if "background_color" in data:
+#         background_color = data["background_color"]
+#     if "navbar_color" in data:
+#         navbar_color = data["navbar_color"]
 
 
 root = tk.Tk()
@@ -45,7 +46,7 @@ frame.place(relwidth=1, relheight=1)
 navbar = tk.Frame(root, bg=navbar_color)
 navbar.place(width=window_width * 0.2, relheight=1)
 
-quitApp = tk.Button(navbar, text="Quit", pady=5, padx=15, fg="white", bd=0, bg="#040f1a", command=quit_app)
-quitApp.pack()
+quit_button = Buttons.QuitButton.quit_button
+quit_button.pack()
 
 root.mainloop()
